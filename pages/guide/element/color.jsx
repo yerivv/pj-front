84c5637bf6d8d76f28f544c71ee7c5678cd9d606
yeri <yeri_v_v@naver.com>
2023-11-09 13:49:46 +0900

    'pj'

---
+++ pages/guide/element/color.jsx
@@ -0,0 +1,17 @@
+export default function ElementColor() {
+  return (
+    <div className="guide-content">
+      <div className="ele-color">
+        <h3>Basic</h3>
+        <ul>
+          <li><i className="chip"></i>Default</li>
+          <li><i className="chip"></i>Default</li>
+          <li><i className="chip"></i>Default</li>
+          <li><i className="chip"></i>Default</li>
+          <li><i className="chip"></i>Default</li>
+          <li><i className="chip"></i>Default</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
---
+++ pages/guide/index.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function GuideIndex() {
+  return (
+    <div>
+      <div>111###</div>
+      <Link href="/guide/element/color">
+        색상이동
+      </Link>
+    </div>
+  )
+}
