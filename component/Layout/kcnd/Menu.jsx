95fda56be07366d7943260daec23bd01e90b3f84
yeri-home <yeri_v_v@naver.com>
2023-11-27 21:47:00 +0900

    mainvisual

--- component/Layout/kcnd/Menu.jsx
+++ component/Layout/kcnd/Menu.jsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
-
 const Menu = ({ small }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [categories, setCategories] = useState(null);
--- pages/kcnd/index.jsx
+++ pages/kcnd/index.jsx
@@ -1,10 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
 import Merchandise from "../../component/organisms/Merchandise";
 
-const KcndIndex = () => {
+export async function getServerSideProps(context) {
+  const res = await fetch('http://localhost:3001/api/mainvisual');
+  const data = await res.json();
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { data },
+  };
+}
+
+const KcndIndex = ({ data }) => {
   return (
     <div className="kcnd-page">
-      <div className="section-full">222</div>
-      <div className="section merchandise-wrap">
+      <div className="section-full" style={{display: 'none'}}>
+    {data.map((item) => (
+      <div key={`mainVisual-${item.id}`} style={{backgroundColor: `${item.bgColor}`}}>
+        <Link href={item.link}>
+          <a>
+          <Image src={item.imgPc} width={1600} height={780} priority alt="메인 배너1" />
+          <span>{item.group}</span>
+          <span>{item.title}</span>
+          <span>{item.desc}</span>
+          </a>
+        </Link>
+      </div>
+    ))}
+      </div>
+      <div className="section merchandise-wrap" style={{marginTop: '40px'}}>
       <Merchandise data="event" />
       </div>
     </div>
