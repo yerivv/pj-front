2704d588712808a20bf793c73ff71565219779cf
yeri-home <yeri_v_v@naver.com>
2023-12-19 22:02:45 +0900

    뒤로가기-브라우저 뒤로가기 제어

--- component/BackButton.jsx
+++ component/BackButton.jsx
@@ -1,20 +1,31 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const BackButton = () => {
   const router = useRouter();
   const currentURL = router.asPath;
   console.log('asPath : ',currentURL);
 
-  const handelHistoryBack = () => {
-    if(currentURL == '/order/success') {
-      router.replace('/');
-    } 
-    // else if() {
+  useEffect(() => {
+    const handleHistoryChange = () => {
+      // 현재 경로가 '/order/success'이면 특정 경로로 이동
+      if (router.asPath === '/order/success') {
+        router.push('/'); // 원하는 경로로 변경
+      }
+    };
 
-    // } 
-    else {
-      router.back();
-    }
+    // 이벤트 리스너 등록
+    window.addEventListener('popstate', handleHistoryChange);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('popstate', handleHistoryChange);
+    };
+  }, [router]);
+
+
+  const handelHistoryBack = () => {
+    router.back();
   }
   return (
     <button onClick={handelHistoryBack}>이전화면</button>
